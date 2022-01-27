5ab8f1be21169afc07d549ca0d0cb023050aa7e3
Roshan Rai <rrai@lb.com>
2022-01-27 10:41:20 +0530

    Build Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        compose_version = '1.1.0-rc01'
-    }
+
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.4"
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0'
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val appId = "com.roshan.calorietracker"
+    const val compileSdk = 31
+    const val minSdk = 26
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
