d14ab596aa15496a8d5148904bed43149599030f
Roshan Rai <rrai@lb.com>
2022-01-27 11:26:02 +0530

    Core UI module setup

--- app/src/main/java/com/roshan/calorietracker/ui/theme/Theme.kt
+++ app/src/main/java/com/roshan/calorietracker/ui/theme/Theme.kt
@@ -5,7 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import com.roshan.core_ui.Dimensions
+import com.roshan.core_ui.LocalSpacing
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
@@ -41,11 +44,13 @@ fun CalorieTrackerTheme(
     } else {
         LightColorPalette
     }
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
 }
