d8563793b7a5f26eb090b96706262876fb35f3e3
Roshan Rai <rrai@lb.com>
2022-01-27 11:13:28 +0530

    Core UI module setup

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,7 +1,9 @@
 object Modules {
+
     const val app = ":app"
 
     const val core = ":core"
+    const val coreUI= ":core-ui"
 
     const val onboardingDomain = ":onboarding:onboarding_domain"
     const val onboardingPresentation = ":onboarding:onboarding_presentation"
