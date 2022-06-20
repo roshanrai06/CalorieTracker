88b055ef47d495197996454e2f23d066654c5c9b
Roshan Rai <roshanrai06@gmail.com>
2022-06-21 05:20:09 +0530

    Library Update

--- buildSrc/src/main/kotlin/AndroidX.kt
+++ buildSrc/src/main/kotlin/AndroidX.kt
@@ -1,5 +1,5 @@
 object AndroidX {
-    private const val coreKtxVersion = "1.7.0"
+    private const val coreKtxVersion = "1.8.0"
     const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
 
     private const val appCompatVersion = "1.4.0"
