537c4972de47f9f042e751d9a0d19bd34a6303aa
Roshan Rai <roshanrai06@gmail.com>
2022-06-21 05:57:52 +0530

    Search ViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/roshan/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.roshan.tracker_presentation.search
+
+import com.roshan.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
