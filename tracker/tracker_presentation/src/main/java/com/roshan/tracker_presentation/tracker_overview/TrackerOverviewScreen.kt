42b6c2d294fdcfb2b5bde1b2f047a94f9e49ed74
Roshan Rai <roshanrai06@gmail.com>
2022-06-21 07:57:29 +0530

    Bug Fixes 1

--- tracker/tracker_presentation/src/main/java/com/roshan/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/roshan/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -56,7 +56,8 @@ fun TrackerOverviewScreen(
                             .fillMaxWidth()
                             .padding(horizontal = spacing.spaceSmall)
                     ) {
-                        state.trackedFoods.forEach { food ->
+                        val foods = state.trackedFoods.filter { it.mealType == meal.mealType }
+                        foods.forEach { food ->
                             TrackedFoodItem(
                                 trackedFood = food,
                                 onDeleteClick = {
